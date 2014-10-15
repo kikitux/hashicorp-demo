0df2798c21706dd1a8fb79f1f66d5bd3472989ce
Alvaro Miranda <kikitux@gmail.com>
2014-10-15 16:58:50 -0700

    update consul2

---
+++ consul2/host/curl_haprox1.sh
@@ -0,0 +1,8 @@
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
+curl http://192.168.11.11:8000 2>/dev/null
---
+++ consul2/host/curl_haprox2.sh
@@ -0,0 +1,8 @@
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
+curl http://192.168.12.12:8000 2>/dev/null
